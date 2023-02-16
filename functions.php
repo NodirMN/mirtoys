46753f1b6379de7e48eb71345ff7bd605f8549f8
Nodir <998977079405@mail.ru>
2023-02-16 11:33:20 +0500

    thema was update

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action('wp_enqueue_scripts', 'nodir_style');
 add_action('wp_enqueue_scripts', 'nodir_scripts');
-add_filter('upload_mimes', 'svg_upload_allow');
+
 
 
 
@@ -19,31 +19,10 @@ function nodir_scripts()
     wp_enqueue_script('nodir-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array('jquery'), null, true);
 };
 
-function svg_upload_allow($mimes)
-{
-    $mimes['svg']  = 'image/svg+xml';
-    return $mimes;
-}
 
-add_filter('wp_check_filetype_and_ext', 'fix_svg_mime_type', 10, 5);
 
-function fix_svg_mime_type($data, $file, $filename, $mimes, $real_mime = '')
-{
 
-    // WP 5.1 +
-    if (version_compare($GLOBALS['wp_version'], '5.1.0', '>='))
-        $dosvg = in_array($real_mime, ['image/svg', 'image/svg+xml']);
-    else
-        $dosvg = ('.svg' === strtolower(substr($filename, -4)));
 
-    if ($dosvg) {
-        if (current_user_can('manage_options')) {
-            $data['ext']  = 'svg';
-            $data['type'] = 'image/svg+xml';
-        } else {
-            $data['ext'] = $type_and_ext['type'] = false;
-        }
-    }
-    return $data;
-}
+    add_theme_support( 'custom-logo' );
+
 ?>
