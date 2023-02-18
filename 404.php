a434a60bbec5745b33cfc6fb52f11c013b47ba39
Nodir <998977079405@mail.ru>
2023-02-18 18:15:05 +0500

    theme was update

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+    get_header();
+?>
+    <div class="container toys">
+            <h2 class="subtitle">
+                <a href="<?php echo get_home_url(); ?>">Page not found</a>
+            </h2>
+    </div>
+
+<?php 
+    get_footer();
+?>
