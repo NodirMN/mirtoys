70e24b2fe263ed1526be7792849781e763d007ec
Nodir <998977079405@mail.ru>
2023-02-18 13:26:20 +0500

    theme update

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action('wp_enqueue_scripts', 'nodir_scripts');
 function nodir_style()
 {
     wp_enqueue_style('nodir-style', get_stylesheet_uri());
-    // wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/styles/main.min.css');
+    wp_enqueue_style('header-style', get_template_directory_uri() . '/assets/styles/style-main.css');
 
 
 };
@@ -30,8 +30,9 @@ function filter_nav_menu_link_attributes($atts, $item, $args)
 {
     if ($args->menu === 'Main') {
         $atts['class'] = 'header__nav-item';
-
         if ($item->current) {
+            $atts['class'] .= ' header__nav-item-active';
+        }if ($item->ID === 855 && (in_category('soft_toys') || in_category('edu_toys'))) {
             $atts['class'] .= ' header__nav-item-active';
         }
     };
